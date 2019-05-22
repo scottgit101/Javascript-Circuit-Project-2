97259b33ade23299226909b54150278eb2801aeb
scottgit101 <45050002+scottgit101@users.noreply.github.com>
2019-05-21 23:29:10 -0500

    Add files via upload

---
+++ Lesson 1.js
@@ -0,0 +1,3 @@
+var x, number1 = 4, number2 = 8, number3 = 10, number4 = 8;
+
+console.log(x === null);
---
+++ Lesson 3.js
@@ -0,0 +1,26 @@
+
+
+var x = 0;
+var greeting = "";
+
+
+
+while (x < 10) {
+
+
+if (x % 2 === 0) {greeting += 'hello ';
+    
+  }
+  else
+  {
+  greeting += 'goodbye ';
+    
+  }
+    x += 1;
+}
+
+console.log(greeting);
+  
+
+ 
+
